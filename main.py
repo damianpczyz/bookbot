03b9c226a08bf93307b36a87904c81af860d6078
damianpczyz <czyz.p.damian@gmail.com>
2025-07-24 16:39:47 +0200

    Parametrize filename with args.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from stats import get_char_num, get_num_words, prepare_report
+from sys import argv, exit
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as book:
         book_content = book.read()
     return book_content    
 
+def show_help() -> None:
+    print('Usage:\n\n'
+          'python3 main.py <path_to_book>')
+
 def main():
-    filepath = "books/frankenstein.txt"
-    book_content = get_book_text(filepath)
-    num_of_words = get_num_words(book_content)
 
-    # print(f'{num_of_words} words found in the document')
-    char_num_dict = get_char_num(book_content)
+    if(len(argv) != 2):
+        show_help()
+        exit(1)    
+    else:
+        filepath = argv[1]
+        book_content = get_book_text(filepath)
+        num_of_words = get_num_words(book_content)
+        char_num_dict = get_char_num(book_content)
 
-    prepare_report(char_num_dict, num_of_words, filepath)
+        prepare_report(char_num_dict, num_of_words, filepath)
 
 main()
